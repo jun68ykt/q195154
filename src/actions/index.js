6d6f1dd6d121a217613c0dc4a21ff2af032d871f
jun68ykt <jun68ykt@teratail>
2019-06-16 23:42:33 +0900

    mockableに作成したURLに変更

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import { GET_GENDERS } from "./types";
 
 export const getGenders = () => async dispatch => {
   try {
-    const response = await axios.get("http://localhost:3090/gender");
+    const response = await axios.get("http://demo5456725.mockable.io/teratail/q195154/gender");
     dispatch({ type: GET_GENDERS, payload: response.data });
   } catch (e) {
     console.log("error");
